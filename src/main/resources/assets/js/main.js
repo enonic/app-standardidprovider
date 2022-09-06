1e7a03809a47a154f038d1bd65ba87d01fdabe54
ashklianko <asl@enonic.com>
2022-09-06 16:13:57 +0200

    Fix setting CONFIG via inline script #482

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -1,6 +1,33 @@
 'use strict' // eslint-disable-line
 
-require('../styles/main.less');
-require('./welcome');
-require('./login');
-require('./creation');
+async function loadConfig() {
+    document.addEventListener('DOMContentLoaded', () => {
+        setTimeout(() => {
+            const backImage = document.getElementById('background-image');
+
+            if (backImage) {
+                backImage.classList.remove('empty');
+            }
+        }, 100);
+    });
+
+    const attr = 'data-config-service-url';
+    const configServiceUrl = document.currentScript.getAttribute(attr);
+    const response = await fetch(configServiceUrl);
+
+    if (!response.ok) {
+        const msg = 'Could not fetch required app config:';
+        throw new Error(`${msg} ${response.status}`);
+    }
+
+    const data = await response.json();
+    data.messages = JSON.parse(data.messages);
+    window.CONFIG = data;
+}
+
+loadConfig().finally(() => {
+    require('../styles/main.less');
+    require('./welcome');
+    require('./login');
+    require('./creation');
+});
