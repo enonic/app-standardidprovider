290f776d9faa86692566c10518546c5dc1857d75
Anatol Sialitski <asi@enonic.com>
2024-06-27 10:48:42 +0200

    No need for absolute url #807

--- src/main/resources/lib/config.js
+++ src/main/resources/lib/config.js
@@ -10,9 +10,7 @@ exports.isLoginWithoutUserEnabled = function() {
 exports.getConfig = () => {
     return {
         idProviderKey: portalLib.getIdProviderKey(),
-        idProviderUrl: portalLib.idProviderUrl({
-            type: 'absolute',
-        }),
+        idProviderUrl: portalLib.idProviderUrl(),
         messages: JSON.parse(adminLib.getPhrases())
     }
 }
