6e22125929568f953d34408d09e39cce0c16c517
alansemenov <ase@enonic.com>
2026-07-01 10:46:09 +0200

    Don't show xp version on the login screen #1365

--- src/main/resources/lib/config.ts
+++ src/main/resources/lib/config.ts
@@ -1,7 +1,7 @@
 import { getIdProviderKey, idProviderUrl } from '/lib/xp/portal';
 // @ts-ignore
 import { getPhrases } from '/lib/xp/i18n';
-import { getInstallation, getVersion } from '/lib/xp/admin';
+import { getInstallation } from '/lib/xp/admin';
 import { Request } from '@enonic-types/core';
 
 type RequestExt = Request & {
@@ -12,7 +12,6 @@ interface Config {
     idProviderKey: string | null;
     idProviderUrl: string;
     installation: string;
-    xpVersion: string;
     messages: Record<string, unknown>; // TODO: not perfect, but good enough for now
     redirectUrl?: string;
 }
@@ -32,6 +31,5 @@ export const getConfig = (req: RequestExt): Config => ({
     idProviderKey: getIdProviderKey(),
     idProviderUrl: idProviderUrl({}),
     installation: getInstallation() || 'Enonic XP',
-    xpVersion: getVersion(),
     messages: getPhrases(req.locales, ['i18n/phrases'])
 });
